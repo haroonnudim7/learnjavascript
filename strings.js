e6c7752a2a6d43f237197452aea24361c2d6870c
haroon nudim <haroon.nudim@bhrish.com>
2025-09-24 14:53:26 +0100

    updated strings.js

--- strings.js
+++ strings.js
@@ -1,2 +1,5 @@
-let message = "Hello, Sunshine!";
-console.log(message); // Hello, Sunshine!
+let message = "Hello " + "and " + "Goodbye";
+console.log(message);
+
+message = "Hello Mate!";
+console.log("This message is for you:", message);
