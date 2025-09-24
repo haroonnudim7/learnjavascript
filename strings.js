55151689b538fb784255d2f346ff1da1cd7ec095
haroon nudim <haroon.nudim@bhrish.com>
2025-09-24 15:07:13 +0100

    add strings.js

--- strings.js
+++ strings.js
@@ -3,3 +3,7 @@ console.log(message);
 
 message = "Hello Mate!";
 console.log("This message is for you:", message);
+
+let name = "Haroon";
+let topic = "JavaScript";       
+console.log(name, "is learning", topic);
