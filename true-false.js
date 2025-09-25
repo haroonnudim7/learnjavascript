1d45abca76637346c589c209b6e364921fc10a48
haroon nudim <haroon.nudim@bhrish.com>
2025-09-25 13:31:57 +0100

    add true-false.js

---
+++ true-false.js
@@ -0,0 +1,15 @@
+console.log(9 == 9); // true
+
+console.log(9 != 20); // true
+
+console.log(2 > 10); // false
+
+console.log(2 < 10); // true
+
+console.log(5 >= 10); // false
+
+console.log(10 <= 10); // true
+
+console.log("ABC"=="ABC"); // true
+
+console.log("ABC"=="abc"); // false
