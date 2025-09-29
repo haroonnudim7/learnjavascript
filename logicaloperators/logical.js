d40d18f547acd6ec9ea38596c9b7ab2347d207f6
haroon nudim <haroon.nudim@bhrish.com>
2025-09-29 11:02:38 +0100

    logical.js

--- logicaloperators/logical.js
+++ logicaloperators/logical.js
@@ -1,6 +1,6 @@
-console.log(true && false);
+console.log(true && false); // returns true if both operands are true
 
-console.log(false || false);
+console.log(false || false); // returns true if at least one operand is true
 
-console.log(!true);
+console.log(!true); // returns opposite boolean value
 
