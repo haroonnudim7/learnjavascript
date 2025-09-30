149011df8d1163a699e1abe3b2173cc486544f6a
haroon nudim <haroon.nudim@bhrish.com>
2025-09-30 12:32:29 +0100

    exercise#4.js

--- exercise#4.js
+++ exercise#4.js
@@ -1,3 +1,8 @@
 let colours = ['red', 'green', 'blue'];
 colours.push('black');
 console.log(colours);
+colours.shift();
+[colours[0], colours[1]] = [colours[1], colours[0]];
+console.log(colours);
+colours.unshift('yellow');
+console.log(colours);
