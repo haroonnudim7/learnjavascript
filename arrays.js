f6f451e29434123f28972bc76b506b9a243b2bd2
haroon nudim <haroon.nudim@bhrish.com>
2025-09-30 10:55:45 +0100

    arrays.js

--- arrays.js
+++ arrays.js
@@ -8,3 +8,18 @@ console.log(lunch);
 
 let mixedArray = ['Bird', true, 10, 5.17]
 console.log(mixedArray);
+
+// Add an element to the end of an array
+let colours = ['red', 'blue', 'green'];
+colours.push('yellow');
+console.log(colours);
+
+// Remove the last element from an array
+let numbers = [1, 2, 3, 4, 5];
+numbers.pop();
+console.log(numbers);
+
+// Add an element to the beginning of an array
+let fishes = ['shark', 'salmon', 'tuna'];
+fishes.unshift('goldfish');
+console.log(fishes);
