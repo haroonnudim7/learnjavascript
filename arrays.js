01a2ae8b63c62c76cae58d1d62f372c55200cc94
haroon nudim <haroon.nudim@bhrish.com>
2025-09-30 10:42:38 +0100

    arrays.js

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 let birds = ['sparrow', 'eagle', 'parrot', 'penguin'];
+console.log(birds[0]); // Owl
+console.log(birds[1]); // Eagle
+
+let lunch = ['burger', 'chips', 'salad'];
+lunch[2] = 'fruit';
+console.log(lunch);
+
+let mixedArray = ['Bird', true, 10, 5.17]
+console.log(mixedArray);
