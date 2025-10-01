90cade4d58bae0f4dcc7499df33550283cefe9ac
haroon nudim <haroon.nudim@bhrish.com>
2025-10-01 10:53:18 +0100

    switchcase.js

---
+++ switchcase.js
@@ -0,0 +1,17 @@
+let age = 15;
+switch (15) {
+    case 10:
+        console.log("Age is 10");
+        break;
+        case 20:
+            console.log("Age is 20");
+            break;
+            default:
+                console.log("Age is neither 10 nor 20");
+}
+
+let num = 1;
+switch (num) {
+    case 1:
+        console.log("Hello World");
+}
