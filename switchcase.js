ee20e0e4b9d2fdf4fd9e9b477f5b4b3e3e17437d
haroon nudim <haroon.nudim@bhrish.com>
2025-10-01 14:29:29 +0100

    switchcase.js

--- switchcase.js
+++ switchcase.js
@@ -21,3 +21,30 @@ switch (num === 0) {
   default:
     console.log("No matching case");
 }
+
+let weekdayNumber = 1;
+switch (weekdayNumber) {
+    case 0:
+        console.log("Sunday");
+        break;
+        case 1:
+            console.log("Monday");
+            break;
+            case 2:
+                console.log("Tuesday");
+                break;
+                case 3:
+                    console.log("Wednesday");
+                    break;  
+                    case 4:
+                        console.log("Thursday");
+                        break;
+                        case 5:
+                            console.log("Friday");
+                            break;
+                            case 6:
+                                console.log("Saturday");
+                                break;
+                                default:
+                                    console.log("The weekday number is invalid");
+}
