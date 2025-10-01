b70230a8dc205c7c4fc77be2776beba42ec4c0b4
haroon nudim <haroon.nudim@bhrish.com>
2025-10-01 10:31:10 +0100

    remove files

--- exercise#4.js
+++
@@ -1,8 +0,0 @@
-let colours = ['red', 'green', 'blue'];
-colours.push('black');
-console.log(colours);
-colours.shift();
-[colours[0], colours[1]] = [colours[1], colours[0]];
-console.log(colours);
-colours.unshift('yellow');
-console.log(colours);
