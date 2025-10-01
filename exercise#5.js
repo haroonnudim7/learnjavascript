c47415d19a4ba3b7ece2f44fbf9283a2d3b6f046
haroon nudim <haroon.nudim@bhrish.com>
2025-10-01 15:19:38 +0100

    exercise#5.js

---
+++ exercise#5.js
@@ -0,0 +1,14 @@
+let grade = "A";
+
+switch (grade) {
+    case "A":
+        console.log("You got an A, so here's a Chocolate for you!");
+        break;
+        case "B":
+            console.log("You got a B, here's a Cookie for you!");
+            break
+            case "C":
+                console.log("You got a C, there's room for improvement and here's your Candy!");
+                default:
+                    console.log("No reward to give");
+}
