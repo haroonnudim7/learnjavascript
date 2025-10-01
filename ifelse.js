a871d0ea126e45a572dbbbec4baa2888ab612a74
haroon nudim <haroon.nudim@bhrish.com>
2025-10-01 14:29:24 +0100

    ifelse.js

--- ifelse.js
+++ ifelse.js
@@ -8,3 +8,14 @@ if (balance > 5000) {
   console.log("Sorry, not enough money. Save more!");
 }
 console.log("The end!");
+
+let colours = "car";
+if (colours === "red") {
+    console.log("The colour is red");
+} else if (colours === "blue") {
+    console.log("The colour is blue");
+} else if (colours === "yellow") {
+    console.log("The colour is yellow");
+} else if (colours === "pink") {
+    console.log ("The colour is pink");
+} else { console.log ("No matching colours")}
