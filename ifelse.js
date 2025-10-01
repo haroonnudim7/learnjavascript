8a1eab0005feae0c5eabebcefd6615c672f969ea
haroon nudim <haroon.nudim@bhrish.com>
2025-10-01 10:29:53 +0100

    Add ifelse.js to controlflows branch

---
+++ ifelse.js
@@ -0,0 +1,10 @@
+let balance = 7000;
+
+if (balance > 5000) {
+  console.log("You have the money for this trip. Let's go!");
+} else if (balance > 3000) {
+  console.log("You only have enough money for a staycation");
+} else {
+  console.log("Sorry, not enough money. Save more!");
+}
+console.log("The end!");
