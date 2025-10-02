d474d4c8d5e948d0b5714e8a91a69f841e36dbf3
haroon nudim <haroon.nudim@bhrish.com>
2025-10-02 11:13:35 +0100

    whilestatement.js

--- whilestatement.js
+++ whilestatement.js
@@ -4,3 +4,15 @@ while (i < 6) {
     console.log(`The value of i = ${i}`);
     i++;
 }
+
+// Heads and Tails While Loop
+
+let flips = 0;
+let isHeads = false;
+
+while(!isHeads) {
+    flips++;
+    isHeads = Math.random() < 0.5;
+}
+
+console.log(`It took ${flips} flips to get heads 5 times.`);
