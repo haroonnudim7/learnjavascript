9b98826b024ca8d1e589e207b3690823e34d2e74
haroon nudim <haroon.nudim@bhrish.com>
2025-10-08 15:33:55 +0100

    object.js

---
+++ object.js
@@ -0,0 +1,8 @@
+let myBook = {
+  title: "JavaScript Introduction",
+  author: "Nathan Sebhastian",
+  describe: function () {
+    console.log(`Book title: ${this.title}`);
+    console.log(`Book author: ${this.author}`);
+  },
+};
