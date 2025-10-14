d566d8ad33ab0ff9496250662c4af74cb43d30ba
haroon nudim <haroon.nudim@bhrish.com>
2025-10-14 10:41:33 +0100

    exercise#8.js

---
+++ exercise#8.js
@@ -0,0 +1,9 @@
+let person = {
+    name: "Haroon Nudim",
+    age: 23,
+    greet: function() {
+        console.log(`Hello! My name is ${name} and I'm ${age} years old.`);
+    }
+};
+
+person.greet();
